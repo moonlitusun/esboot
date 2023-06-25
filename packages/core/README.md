e3760bc1040d71118413e59ebf2d5ff92fbe9fdd
Roc <moonlitusun@gmail.com>
2023-06-25 17:36:01 +0800

    docs: 添加文档 项目

--- packages/core/README.md
+++ packages/core/README.md
@@ -1,5 +1,7 @@
 # v2 RoadMap
 
+- [ ] wins上面Bridge mock 端口无效
+- [ ] DeprecationWarning
 - [ ] 兼容多平台
 - [ ] 写文档
 - [ ] 支持alias
---
+++ packages/docs/.umirc.ts
@@ -0,0 +1,15 @@
+export default {
+  favicons: [
+    'https://img.alicdn.com/tfs/TB1YHEpwUT1gK0jSZFhXXaAtVXa-28-27.svg',
+  ],
+  mfsu: false,
+  routePrefetch: {},
+  manifest: {},
+  plugins: ['@umijs/plugin-docs'],
+  conventionRoutes: {
+    exclude: [/\/components\//],
+  },
+  alias: {
+    '@': './src'
+  }
+};
