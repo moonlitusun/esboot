a275deea5193d1b5f7174b98a1223dee7ee4a05c
Roc <moonlitusun@gmail.com>
2023-09-06 10:07:20 +0800

    feat: update pc template

--- packages/create-esboot/templates/mp/src/platforms/mobile/_browser/modules/index/app.tsx
+++ packages/create-esboot/templates/mp/src/platforms/mobile/_browser/modules/index/app.tsx
@@ -1,12 +1,10 @@
 import { Outlet } from 'react-router-dom';
-import { FONT_SIZE } from '@/constants/config';
 
 import './app.scss';
 
 const App = () => (
   <div styleName="main">
     Mobile Browser Platform
-    {FONT_SIZE}
     <Outlet />
   </div>
 );
