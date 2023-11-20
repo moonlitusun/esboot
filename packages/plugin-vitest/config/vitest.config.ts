29a7a14bdf0d1a7eb56f989cde9028af8d829a3f
Roc <moonlitusun@gmail.com>
2023-11-20 22:00:50 +0800

    chore: fix remove plugin path

--- packages/plugin-vitest/config/vitest.config.ts
+++ packages/plugin-vitest/config/vitest.config.ts
@@ -1,5 +1,6 @@
 /// <reference types="vitest" />
 import { defineConfig, configDefaults } from 'vitest/config';
+import { join, isAbsolute } from 'path';
 import react from '@vitejs/plugin-react';
 import { esbootConfig, registerTypescript } from '@dz-web/esboot';
 
@@ -7,6 +8,19 @@ import { alias } from '../dist';
 
 registerTypescript();
 esbootConfig.initUserConfig();
+
+const customTSConfigAlias = {};
+
+const { alias: esbootAlias } = esbootConfig.userOpts;
+for (let k in esbootAlias) {
+  const rawValue = esbootAlias[k];
+  const isAbsoluteValue = isAbsolute(rawValue);
+  // FIX: Use Options
+  const key = isAbsoluteValue ? k : `${k}`;
+  const value = isAbsoluteValue ? rawValue : join(process.cwd(), `./${rawValue}`);
+
+  customTSConfigAlias[key] = value;
+}
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -16,7 +30,7 @@ export default defineConfig({
     environment: 'jsdom',
     alias: {
       ...alias,
-      ...esbootConfig.userOpts.alias,
+      ...customTSConfigAlias,
     },
   },
 });
