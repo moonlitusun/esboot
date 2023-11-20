ee90d6b312107aad69aafa0f1719c073ea6af35e
Roc <moonlitusun@gmail.com>
2023-11-20 23:40:46 +0800

    chore: add storybook temp test

---
+++ packages/plugin-storybook/src/helpers/path.ts
@@ -0,0 +1,16 @@
+import { pathExistsSync } from 'fs-extra';
+import { join } from 'path';
+
+export function joinExecPath(path: string) {
+  // pnpm
+  const pnpmPath = join(__dirname, '../../', path);
+  if (pathExistsSync(pnpmPath)) return pnpmPath;
+
+  // bun
+  const bunPath = join(process.cwd(), path);
+  return bunPath;
+}
+
+export function searchCommand(command: string) {
+  return joinExecPath(`./node_modules/.bin/${command}`);
+};
