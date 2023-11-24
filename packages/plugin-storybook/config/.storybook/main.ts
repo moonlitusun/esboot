0468f6f14f772c2161d1e60c80588dabe004ab2f
Roc <moonlitusun@gmail.com>
2023-11-24 11:30:54 +0800

    feat: add @dz-web/esboot-storybook-framework

--- packages/plugin-storybook/config/.storybook/main.ts
+++ packages/plugin-storybook/config/.storybook/main.ts
@@ -9,10 +9,11 @@ import { join, dirname } from 'path';
 function getAbsolutePath(value: string): any {
   return dirname(require.resolve(join(value, 'package.json')));
 }
+
 const config: StorybookConfig = {
   stories: [
-    '../stories/**/*.mdx',
-    '../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)',
+    join(process.cwd(), '/src/stories/**/*.mdx'),
+    join(process.cwd(), '/src/stories/**/*.stories.@(js|jsx|mjs|ts|tsx)'),
   ],
   addons: [
     getAbsolutePath('@storybook/addon-links'),
