db82e78b9aeb955574d54d92cc3724521b4efe8d
Roc <moonlitusun@gmail.com>
2024-08-01 23:39:16 +0800

    feat: del index2.html

--- examples/sp-base/.esbootrc.ts
+++ examples/sp-base/.esbootrc.ts
@@ -3,10 +3,10 @@ import { BundlerVite } from '@dz-web/esboot-bundler-vite';
 import { BundlerWebpack } from '@dz-web/esboot-bundler-webpack';
 import type { BundlerWebpackOptions } from "@dz-web/esboot-bundler-webpack";
 
-export default defineConfig<any>({
-  bundler: BundlerVite,
-  // bundler: BundlerWebpack as any,
-  // bundlerOptions: { mfsu: false },
+export default defineConfig<BundlerWebpackOptions>({
+  // bundler: BundlerVite,
+  bundler: BundlerWebpack,
+  bundlerOptions: { mfsu: false },
   sourceMap: false,
   alias: {
     '@@': 'src'
