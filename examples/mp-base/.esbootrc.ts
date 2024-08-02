e9e8f6cd9be8ba7c5146224be41b9f98160f7d04
Roc <moonlitusun@gmail.com>
2024-08-03 00:25:20 +0800

    feat: sync mp-tpl

--- examples/mp-base/.esbootrc.ts
+++ examples/mp-base/.esbootrc.ts
@@ -2,22 +2,29 @@ import { defineConfig } from '@dz-web/esboot';
 import { BundlerWebpack } from '@dz-web/esboot-bundler-webpack';
 import type { BundlerWebpackOptions } from "@dz-web/esboot-bundler-webpack";
 
-export default defineConfig<BundlerWebpackOptions>({
-  bundler: BundlerWebpack as any,
-  bundlerOptions: { mfsu: false },
-  server: {
-    port: 8111,
-  }
-  // analyze: true,
-  // extraBabelIncludes: [
-  //   /filter-obj/i,
-  //   /immer/i,
-  //   /zustand/i,
-  //   /query-string/i,
-  //   /react-intl/i,
-  //   /d3-/i,
-  //   /@tanstack/i,
-  //   /@react-spring/i,
-  //   /@floating-ui/i,
-  // ],
+export default defineConfig<BundlerWebpackOptions>((cfg) => {
+  console.log(cfg, '<-- cfg');
+  return {
+    bundler: BundlerWebpack as any,
+    bundlerOptions: { mfsu: false },
+    server: {
+      port: 8111,
+    },
+    define: {
+      'process.env.isMobile': cfg.isMobile as any,
+      'process.env.isBrowser': cfg.isBrowser as any,
+    },
+    // analyze: true,
+    // extraBabelIncludes: [
+    //   /filter-obj/i,
+    //   /immer/i,
+    //   /zustand/i,
+    //   /query-string/i,
+    //   /react-intl/i,
+    //   /d3-/i,
+    //   /@tanstack/i,
+    //   /@react-spring/i,
+    //   /@floating-ui/i,
+    // ],
+  };
 });
