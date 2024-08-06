091931f9bd1691ed0a7771472fcf3dff848724c3
Roc <moonlitusun@gmail.com>
2024-08-06 23:47:27 +0800

    feat: add commitlint

--- packages/esboot/src/cli/prepare/index.ts
+++ packages/esboot/src/cli/prepare/index.ts
@@ -1,8 +1,9 @@
 import { generateTypeScriptCfg } from './generate-typescript-cfg';
-import { generateStylelintCfg } from "./generate-stylelint-cfg";
-import { generateESLintCfg } from "./generate-eslint-cfg";
-import { generatePrettierCfg } from "./generate-prettier-cfg";
-import { generateCommitlintCfg } from "./generate-commitlint-cfg";
+import { generateStylelintCfg } from './generate-stylelint-cfg';
+import { generateESLintCfg } from './generate-eslint-cfg';
+import { generatePrettierCfg } from './generate-prettier-cfg';
+import { generateCommitlintCfg } from './generate-commitlint-cfg';
+
 const basePath = '../../config';
 export function prepare() {
   generateTypeScriptCfg(basePath);
