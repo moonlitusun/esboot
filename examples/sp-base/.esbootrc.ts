96a5202c62403d6bc635984a64714a0e496e052d
Roc <moonlitusun@gmail.com>
2024-08-22 09:12:38 +0800

    chore: del pnpm patch apply cmd

--- examples/sp-base/.esbootrc.ts
+++ examples/sp-base/.esbootrc.ts
@@ -1,6 +1,6 @@
 import { defineConfig, type Configuration } from '@dz-web/esboot';
-// import { BundlerVite as Bundler, type BundlerViteOptions as BundlerOptions  } from '@dz-web/esboot-bundler-vite';
-import { BundlerWebpack as Bundler, type BundlerWebpackOptions as BundlerOptions } from '@dz-web/esboot-bundler-webpack';
+import { BundlerVite as Bundler, type BundlerViteOptions as BundlerOptions  } from '@dz-web/esboot-bundler-vite';
+// import { BundlerWebpack as Bundler, type BundlerWebpackOptions as BundlerOptions } from '@dz-web/esboot-bundler-webpack';
 
 export default defineConfig<BundlerOptions>({
   bundler: Bundler,
