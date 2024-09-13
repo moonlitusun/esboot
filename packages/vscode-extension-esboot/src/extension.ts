e4c34d96ddfbbb1b1db6f11591717f6bd746cfce
Roc <moonlitusun@gmail.com>
2024-09-13 10:52:42 +0800

    feat: not activate when not esboot project

--- packages/vscode-extension-esboot/src/extension.ts
+++ packages/vscode-extension-esboot/src/extension.ts
@@ -1,8 +1,21 @@
 import * as vscode from 'vscode';
-
+import fse from 'fs-extra';
+import path from 'path';
+import { root } from './utils';
 import { activateSidebar } from './sidebar';
 
 export function activate(context: vscode.ExtensionContext) {
+  if (!root) return;
+
+  const cfgPath = path.join(root, '.esbootrc.ts');
+  if (!fse.existsSync(cfgPath)) return;
+
+  // .esbootrc.ts 是否包含isSP: true
+  const content = fse.readFileSync(cfgPath, 'utf8');
+  const isSP = content.includes('isSP: true');
+  if (isSP) return;
+
+  console.log('ESBoot activated');
   activateSidebar(context);
 }
 
