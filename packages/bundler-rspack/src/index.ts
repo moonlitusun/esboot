43fd089b032cf36a32efed1a124bf90d259dea71
Roc <moonlitusun@gmail.com>
2024-09-25 11:15:16 +0800

    chore: prerelease 14

--- packages/bundler-rspack/src/index.ts
+++ packages/bundler-rspack/src/index.ts
@@ -1,37 +1,37 @@
-import { rspack } from '@rspack/core';
+// import { rspack } from '@rspack/core';
 
 import { Bundler } from '@dz-web/esboot';
 
-import { getRspackCfg } from './cfg';
+// import { getRspackCfg } from './cfg';
 
 export class BundlerRspack extends Bundler {
   async dev() {
-    const rspackCfg = await getRspackCfg(this.cfg);
-    const compiler = rspack(rspackCfg);
-    const watching = compiler.watch(
-      {
-        // Example
-        aggregateTimeout: 300,
-        poll: undefined,
-      },
-      (err, stats) => {
-        // Print watch/build result here...
-        console.log(stats);
-      }
-    );
+    // const rspackCfg = await getRspackCfg(this.cfg);
+    // const compiler = rspack(rspackCfg);
+    // const watching = compiler.watch(
+    //   {
+    //     // Example
+    //     aggregateTimeout: 300,
+    //     poll: undefined,
+    //   },
+    //   (err, stats) => {
+    //     // Print watch/build result here...
+    //     console.log(stats);
+    //   }
+    // );
   }
 
   async build() {
-    const rspackCfg = await getRspackCfg(this.cfg);
-    const compiler = rspack(rspackCfg);
+    // const rspackCfg = await getRspackCfg(this.cfg);
+    // const compiler = rspack(rspackCfg);
 
-    compiler.run((err, stats) => {
-      // ...
+    // compiler.run((err, stats) => {
+    //   // ...
 
-      compiler.close((closeErr) => {
-        // ...
-      });
-    });
+    //   compiler.close((closeErr) => {
+    //     // ...
+    //   });
+    // });
   }
 }
 
