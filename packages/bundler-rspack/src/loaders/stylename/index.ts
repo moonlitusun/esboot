4ca55cc971278b2091a9eda88d3293670387bfe4
Roc <moonlitusun@gmail.com>
2024-10-12 21:49:08 +0800

    feat: init styleName loader

--- packages/bundler-rspack/src/loaders/stylename/index.ts
+++ packages/bundler-rspack/src/loaders/stylename/index.ts
@@ -9,6 +9,9 @@ import { findStyleImports } from './utils';
 
 // FIXME: refactor by rust
 const generateScopedName = generateScopedNameFactory(getCssHashRule());
+const jsxPattern =
+  /(_jsxDEV|_jsx)\([^,]+,\s*({[^}]*styleName:\s*"[^"]*"[^}]*})/g;
+
 export default function (this: any, source: string): string {
   let { imports, updatedSource } = findStyleImports(source);
   if (imports.length === 0) {
@@ -21,44 +24,77 @@ export default function (this: any, source: string): string {
     );
   }
 
-  const { statement, prefixStatement, variable, filepath } = imports[0];
+  const { filepath } = imports[0];
   const stylePath = join(dirname(this.resourcePath), filepath);
 
-  console.log(stylePath, 'stylePath');
-
-  // // console.log('resourcePath', resourcePath);
-  // const className = source.match(/className:\s*"([^"]+)"/)?.[1];
-  // console.log(className, 'className');
   const classNameList: string[] = [];
 
   console.log(source, 'source');
-  updatedSource = source.replace(
-    /styleName:\s*"([^"]+)"/g,
-    (match, styleNames) => {
-      const classNames = styleNames
-        .split(/\s+/)
-        .map((styleName: string) => {
-          const value = generateScopedName(styleName, stylePath);
 
-          classNameList.push(value);
-          return value;
-        })
-        .join(' ');
+  updatedSource = source.replace(jsxPattern, (match, jsxFunc, objStr) => {
+    // console.log(match, jsxFunc, objStr, '< 000 ');
+    
+    return jsxFunc;
+    // const updatedObjStr = objStr.replace(
+    //   /styleName:\s*"([^"]+)"/g,
+    //   (styleNameMatch, styleNames) => {
+    //     const classNames = styleNames
+    //       .split(/\s+/)
+    //       .map((styleName: string) => generateScopedName(styleName, stylePath))
+    //       .join(' ');
 
-      console.log(classNames, 'classNames');
+    //     // 更新 className
+    //     if (objStr.includes('className:')) {
+    //       objStr = objStr.replace(
+    //         /className:\s*"([^"]*)"/,
+    //         (classNameMatch, existingClassNames) => {
+    //           const combinedClassNames = existingClassNames
+    //             ? `${existingClassNames} ${classNames}`
+    //             : classNames;
+    //           return `className: "${combinedClassNames}"`;
+    //         }
+    //       );
+    //     } else {
+    //       // 如果没有 className，添加一个
+    //       objStr = objStr.replace('}', `, className: "${classNames}"}`);
+    //     }
 
-      return '';
-    }
-  );
+    //     // 移除 styleName
+    //     return '';
+    //   }
+    // );
 
-  updatedSource = source.replace(
-    /className:\s*"([^"]+)"/g,
-    (match, classNames) => {
-      return `className: "${classNames} ${classNameList.join(' ')}"`;
-    }
-  );
+    // 返回更新后的 _jsxDEV 或 _jsx 调用
+    // return `${jsxFunc}(${match.split(',')[1]}, ${updatedObjStr}`;
+  });
 
-  console.log(classNameList, 'classNameList');
+  // updatedSource = source.replace(
+  //   /styleName:\s*"([^"]+)"/g,
+  //   (match, styleNames) => {
+  //     const classNames = styleNames
+  //       .split(/\s+/)
+  //       .map((styleName: string) => {
+  //         const value = generateScopedName(styleName, stylePath);
 
-  return updatedSource;
+  //         classNameList.push(value);
+  //         return value;
+  //       })
+  //       .join(' ');
+
+  //     console.log(classNames, 'classNames');
+
+  //     return '';
+  //   }
+  // );
+
+  // updatedSource = source.replace(
+  //   /className:\s*"([^"]+)"/g,
+  //   (match, classNames) => {
+  //     return `className: "${classNames} ${classNameList.join(' ')}"`;
+  //   }
+  // );
+
+  // console.log(classNameList, 'classNameList');
+
+  return source;
 }
