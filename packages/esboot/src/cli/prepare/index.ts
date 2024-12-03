16391151789dc4ada08556b1bde0c6d6351ffb40
Roc <moonlitusun@gmail.com>
2024-12-03 13:21:14 +0800

    feat: improve prepare command when CI

--- packages/esboot/src/cli/prepare/index.ts
+++ packages/esboot/src/cli/prepare/index.ts
@@ -12,23 +12,28 @@ import { generatePrettierCfg } from './generate-prettier-cfg';
 import { generateCommitlintCfg } from './generate-commitlint-cfg';
 import { generateTypeScriptTypes } from './generate-typescript-types';
 import { updateVSCodeSetting } from './update-vscode-setting';
-import { error } from "@dz-web/esboot-common/helpers";
+import { error } from '@dz-web/esboot-common/helpers';
 
 export function prepare() {
-  try {
-    huskySetup({ configRootPath: cfg.config.configRootPath });
-  } catch (err) {
-    error((err as Error).message);
-  }
+
+  const { isCIBuild } = cfg.config;
 
   generateTypeScriptCfg();
-  generateStylelintCfg();
-  generatePrettierCfg();
-  generateESLintCfg();
-  generateCommitlintCfg();
   generateTypeScriptTypes();
 
-  updateVSCodeSetting();
+  if (!isCIBuild) {
+    generateStylelintCfg();
+    generatePrettierCfg();
+    generateESLintCfg();
+    generateCommitlintCfg();
+    updateVSCodeSetting();
+
+    try {
+      huskySetup({ configRootPath: cfg.config.configRootPath });
+    } catch (err) {
+      error((err as Error).message);
+    }
+  }
 
   callPluginHookOfOnlyExec(PluginHooks.prepare, pluginHooksDict, cfg.config);
 }
