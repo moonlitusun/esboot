407095143ed46d5f208f7e5e19c053ef45d13932
Roc <moonlitusun@gmail.com>
2024-12-11 21:12:37 +0800

    feat: support tailwind dark-mode

--- examples/mp-base/src/platforms/pc/modules/test/pages/index/index.tsx
+++ examples/mp-base/src/platforms/pc/modules/test/pages/index/index.tsx
@@ -14,6 +14,7 @@ const Index = genericMemo(function Index() {
         to="/detail"
       >
         go to detail2233
+        <div className="text-[blue] dark:text-[red]">2432432</div>
       </Link>
       <div styleName="w375" className="text-2xl text-red-500">
         375 width in 750 design
--- packages/bundler-common/src/helpers/add-tailwindcss.ts
+++ packages/bundler-common/src/helpers/add-tailwindcss.ts
@@ -6,6 +6,7 @@ import { writeFile } from '@dz-web/esboot-common/fs-extra';
 import type { Config } from 'tailwindcss';
 
 const defaultTailwindCSSOpts: Config = {
+  darkMode: ['selector', '.dz-theme-dark'],
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
     extend: {},
