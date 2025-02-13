140428ea07c30cfc2886c8855b71547bbb1bcce4
Roc <moonlitusun@gmail.com>
2025-02-13 22:45:21 +0800

    feat: when config.js not exist, not copy and import

--- packages/bundler-common/src/helpers/inject-html.ts
+++ packages/bundler-common/src/helpers/inject-html.ts
@@ -1,4 +1,5 @@
 import { join } from 'node:path';
+import { existsSync } from 'node:fs';
 import { isUndefined } from '@dz-web/esboot-common/lodash';
 import type { ConfigurationInstance } from '@dz-web/esboot';
 
@@ -7,19 +8,23 @@ const getVersion = (cwd: string) => {
   return pkg.version;
 };
 
-export const injectHtml = (
+export const injectHtml = async (
   html: string,
   cfg: ConfigurationInstance,
   title?: string
 ) => {
   const { BRIDGE_MOCK_HOST, BRIDGE_MOCK_PORT, BUILD_VERSION } = process.env;
-  const { isBrowser, ipv4, publicPath, isDev, cwd } = cfg.config;
+  const { isBrowser, ipv4, publicPath, isDev, cwd, configJSPath } = cfg.config;
+
+  const isConfigJSExists = existsSync(configJSPath);
 
   const version = BUILD_VERSION || getVersion(cwd);
   const isInjectBridgeMock = !isBrowser && isDev;
 
-  const importCfgScript = `<script src="${publicPath}config.js?v=${version}">
-  <\/script>`;
+  const importCfgScript = isConfigJSExists
+    ? `<script src="${publicPath}config.js?v=${version}">
+  <\/script>`
+    : '';
   const injectBridgeMockScript = isInjectBridgeMock
     ? `<script>
     window.brigeMockHost = "http://${BRIDGE_MOCK_HOST || ipv4}";
@@ -29,7 +34,7 @@ export const injectHtml = (
 
   let _html = html.replace(
     '<body>',
-    `<body>${importCfgScript}${injectBridgeMockScript}`
+    `<body>${isConfigJSExists ? importCfgScript : ''}${injectBridgeMockScript}`
   );
   if (!isUndefined(title))
     _html = _html.replace('<head>', `<head><title>${title}</title>`);
